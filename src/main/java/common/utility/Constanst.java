31308d0ff9cc9877d454e100cb7df4984eb12346
lenga44 <lenga4579@gmail.com>
2023-12-22 00:17:16 +0700

    Innit Constanst class

---
+++ src/main/java/common/utility/Constanst.java
@@ -0,0 +1,10 @@
+package common.utility;
+
+public class Constanst {
+    public static final String PROJECT_PATH = System.getProperty("user.dir");
+
+
+    // System Variables
+    public static final String KEYWORD_FAIL = "FAIL";
+    public static final String KEYWORD_PASS = "PASS";
+}
