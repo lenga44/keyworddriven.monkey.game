5cfbc6f77e839c705ff8d51bfa792533bd0cb378
lenga44 <lenga4579@gmail.com>
2024-07-12 06:46:11 +0700

    Update DinosaurChasing.java

--- src/main/java/common/keywords/app/custom/DinosaurChasing.java
+++ src/main/java/common/keywords/app/custom/DinosaurChasing.java
@@ -7,20 +7,31 @@ import common.keywords.app.verify.IsElement;
 import common.utility.LogicHandle;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class DinosaurChasing {
     public static void maxJump(String locator,String replaceStr,String expected){
         try {
+            boolean elem = false;
             expected= LogicHandle.replaceStr(expected,replaceStr);
+            String[] list = new String[0];
+            if(expected.contains("[")) {
+                list = LogicHandle.convertToArrayListString(expected, "\"").toArray(new String[0]);
+            }
+            if(list.length==0){
+                list[0]=expected;
+            }
             LocalDateTime time = LocalDateTime.now();
             LocalDateTime time1 = time.plusSeconds(60);
             do {
                 Click.clickByPositionPoco(locator);
-                boolean elem = IsElement.elementDisplay(expected);
-                time = LocalDateTime.now();
-                if (elem == true) {
-                    break;
+                for (String item : list) {
+                    elem = IsElement.elementDisplay(item);
+                    if (elem == true) {
+                        break;
+                    }
                 }
+                time = LocalDateTime.now();
             } while (time.compareTo(time1) <= 0);
         }catch (Exception e){
             ExceptionEx.exception(e.getMessage());
